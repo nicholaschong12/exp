559082e6e019a5812137c1f3c1e25ee5f61cc554
Nicholas <nicholaschong12@gmail.com>
2015-07-08 22:07:17 +0800

    Get Post Put Delete

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8009;
+var port = process.env.PORT || 8007;
 
 var router = express.Router();
 
@@ -27,7 +27,7 @@ router.route("/bears")
         
         var bear = new Bear();      // create a new instance of the Bear model
         bear.name = req.body.name;  // set the bears name (comes from the request)
-
+		
         // save the bear and check for errors
         bear.save(function(err) {
             if (err)
@@ -56,13 +56,37 @@ router.route('/bears/:bear_id')
 		});
 	})
 	
-	.put(function(req,res){
-		Bear.findById(req.params.bear_id,function(err,bear){
-				if(err)
-					res.send(err);
-				res.json({message : 'Bear Updated!'});
-		});
-	})
+	.put(function(req, res) {
+
+        // use our bear model to find the bear we want
+        Bear.findById(req.params.bear_id, function(err, bear) {
+
+            if (err)
+                res.send(err);
+
+            bear.name = req.body.name;  // update the bears info
+
+            // save the bear
+            bear.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Bear updated! ' + bear.name });
+            });
+
+        });
+    })
+    
+    .delete(function(req,res){
+	    Bear.remove({
+		    _id: req.params.bear_id
+	    },function(err,bear){
+		    if(err)
+		    	res.send(err);
+		    res.json({message: "Sucessfully deleted "});
+	    });
+    });
+
 
 app.use('/api',router);
 
